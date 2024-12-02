58d6eef27d68a1849500251b825f70f28fef34e8
Jean-Sebastien <jeansebastienbeaulieuscrde.01@gmail.com>
2024-12-02 14:43:15 -0500

    Add initial Flask application setup with environment configuration

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+# ...existing code...
+
+if __name__ == '__main__':
+    app.run()
